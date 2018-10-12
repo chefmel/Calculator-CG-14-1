66c210481917cf5ea79a09fe1eda42ecede23ed1
Melissa DoddRuesing <melissa.doddruesing@gmail.com>
2018-10-12 10:33:03 -0500

    Add project files.

---
+++ Calculato CG 11-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculato_CG_11_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
